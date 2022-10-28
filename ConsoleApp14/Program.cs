0d1c5664a169c4a66e3d745bede25104d46fe922
USER <USER@DESKTOP-TRQG01D>
2022-10-28 12:57:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Выводим числа кратные 5
+            for (byte i = 1; i <= 100; i++) // применение цикла for / объявление локальной переменной i целого типа byte и указание условия
+            {
+                if (i % 5 != 0) continue; // contitnue начинает новую итерацию ближайшего внешнего оператора итерации
+                Console.Write("\t{0}", i); // вывод на экран  строки с вычисленным значением переменной i
+            }
+
+            Console.ReadLine(); // чтение строк
+        }
+    }
+}
